38110dae09561a8a23656dad06786f9a8f0a71f4
Juanjo Rodriguez <juan.rodriguez49@udea.edu.co>
2023-03-19 17:04:55 -0500

    Corrección recomendaciones contenido

--- modelos.py
+++ modelos.py
@@ -25,9 +25,7 @@ dist, idlist = model.kneighbors(dummies)
 distancias=pd.DataFrame(dist)
 id_list=pd.DataFrame(idlist)
 
-movie_name=str(input('Ingrese la película para buscar similares',))
-
-def MovieRecommender(movie_name = list(movies2['title'].value_counts().index)): #Se necesitan las películas de la bd
+def MovieRecommender(movie_name): #Se necesitan las películas de la bd
     movie_list_name = []  #Se define un vector vacío
     movie_id = movies2[movies2['title'] == movie_name].index # Lista de los id de las peliculas
     movie_id = movie_id[0]  #El primer id es 0
@@ -35,6 +33,7 @@ def MovieRecommender(movie_name = list(movies2['title'].value_counts().index)): 
         movie_list_name.append(movies2.loc[newid].title)
     return movie_list_name
 
-
 print(interact(MovieRecommender))
+
+
 
